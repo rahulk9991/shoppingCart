c04e9d159339a58e00f97d5d05357450e2ff4b38
rahulk9991 <rahulkondapally999@gmail.com>
2020-11-22 00:56:50 -0500

    modifications to home and Register

--- partymontreal/app/src/main/java/com/example/partymontreal/Register.java
+++ partymontreal/app/src/main/java/com/example/partymontreal/Register.java
@@ -63,7 +63,7 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this, "User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(),Login.class));
                         }else {
                             Toast.makeText(Register.this, "Error!"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
